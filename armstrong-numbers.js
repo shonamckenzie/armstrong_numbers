69b6e8e51b927b067ba3d65b0ea7bf19b155d401
Shona McKenzie <shona_mckenzie@hotmail.com>
2019-12-10 20:39:41 +0000

    completed function for testing armstrong numbers

--- armstrong-numbers.js
+++ armstrong-numbers.js
@@ -1,17 +1,20 @@
+import { forStatement } from "@babel/types";
+
 //
 // This is only a SKELETON file for the 'Armstrong numbers' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
 
-export const validate = (n) => {
-  const digit = n.toString().length;
-  if (digit == 1){
-    if (n ** digit === n){
-     return true
-    }
-  }else if (digit == 2) {
-    if (n ** digit + n ** digit === n){
-      return true
-    }else return false
-  }
+  export const validate = (n) => {
+  const digits = [...n.toString()];
+  const power = digits.length;
+  let result = 0;
+
+  digits.forEach((digit) =>{
+    result += digit ** power;  
+  })
+  if (result === n){
+    return true;
+  }else return false;
 };
+
